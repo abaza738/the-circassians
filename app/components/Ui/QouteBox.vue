a8f9bc0d0c9a6497dd4cffc53c5040924647405e
AbedShowgan <showganabed@gmail.com>
2025-05-10 00:21:29 +0300

    added qoutes to homepage

--- app/components/Ui/QouteBox.vue
+++ app/components/Ui/QouteBox.vue
@@ -1,12 +1,12 @@
 <template>
-  <blockquote class="relative p-4 mt-4  bg-white rounded-lg shadow-md border-0">
+  <blockquote class="relative p-2 mt-2 mb-2  bg-white rounded-lg shadow-md border-0 h-[220px]">
     <span class="icon-[tabler--quote] text-gray-300 absolute -left-2 -top-2 size-4 rotate-180 rtl:rotate-0"></span>
     <div class="relative z-10">
       <p class="text-gray-800 text-base">
         <em>{{ quote }}</em>
       </p>
     </div>
-    <footer class="pt-2 pb-2">
+    <footer class="pb-2 mt-2">
       <div class="text-gray-500 text-sm font-semibold">~ {{ author }}</div>
     </footer>
   </blockquote>
