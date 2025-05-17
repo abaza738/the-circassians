abed56ea6412ae750934f6f4981a69fa26fffa58
Maher Abaza <maher.abaza@protonmail.com>
2025-05-17 14:47:23 +0300

    fix(og-image): replace jpg image with png

--- app/components/OgImage/Default.vue
+++ app/components/OgImage/Default.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div class="h-full w-full flex border-solid border-[#fdfb3b] border-[12px] text-white">
-    <img src="/images/og-image.jpg" alt="og-image" class="absolute top-0 left-0 w-full h-full z-[0]">
+    <img src="/images/og-image.png" alt="og-image" class="absolute top-0 left-0 w-full h-full z-[0]">
 
     <div class="flex-1 flex flex-col items-center justify-center w-full max-w-2/3 h-full m-auto p-16 text-center z-10">
       <Adiga width="240" height="120" class="text-[#fdfb3b]" />
