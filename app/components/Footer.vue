f9fbe7fffec1c640f75ac4a8d6d9fd1b4e780a81
Maher Abaza <maher.abaza@protonmail.com>
2026-02-06 17:38:25 +0300

    fix(footer): update copyright year to be dynamic

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="grid place-items-center w-full p-6 sm:p-12 border-t-2 border-accent-400 bg-primary-800 text-accent-50">
     <img src="/images/adiga.svg" alt="adiga flag" class="w-[100px] sm:w-[150px]">
-    <h4>The Circassians &copy; 2025</h4>
+    <h4>The Circassians &copy; 2025 - {{ new Date().getFullYear() }}</h4>
   </footer>
 </template>
